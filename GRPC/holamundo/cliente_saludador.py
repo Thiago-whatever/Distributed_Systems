eaf516300eb0915107f9b24c933103cbd66ecb29
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-02-19 17:59:55 -0600

    Se hacen modificaciones

--- GRPC/holamundo/cliente_saludador.py
+++ GRPC/holamundo/cliente_saludador.py
@@ -1,12 +1,12 @@
 import grpc
-import protos.holamundo_pb2
-import protos.holamundo_pb2_grpc
+import GRPC.holamundo.holamundo_pb2 as hm_pb2
+import GRPC.holamundo.holamundo_pb2_grpc as hm_pb2_grpc
 
 def run():
     print(" Intentando decir hola")
     with grpc.insecure_channel("localhost:50501") as channel:
-        stub = holamundo_pb2_grpc.SaludadorStub()
-        respuesta = stub.diHola(holamundo_pb2.HelloRequest(name="Octavio"))
+        stub = hm_pb2_grpc.SaludadorStub()
+        respuesta = stub.diHola(hm_pb2.HelloRequest(name="Octavio"))
         print("El cliente recibio", respuesta.message)
 
 if __name__ == "__main__":
