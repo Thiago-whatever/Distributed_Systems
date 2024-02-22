ed07165e9b40fbc05ff5fc01f7b85177036d020a
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-02-21 18:24:06 -0600

    Modificaciones al servidor comunicaciones

--- gRPCComunicaciones/servidor_comunicaciones.py
+++ gRPCComunicaciones/servidor_comunicaciones.py
@@ -16,7 +16,6 @@ class Comunicador(comunicaciones_pb2_grpc.ComunicadorServicer):
         #hasta terminar de enviar todo
         for elemento in Comunicador.elementos.values:
             yield elemento #Cada que tenga un stream tengo que usar yield
-        pass
 
     def listador_elementos2(self, request, context):
         listado = [] #Poblar lista con todos los elementos
@@ -25,7 +24,26 @@ class Comunicador(comunicaciones_pb2_grpc.ComunicadorServicer):
             listado.append(elemento)
 
         return comunicaciones_pb2.ListadoElementos(objetos = listado)
-        pass
+        
 
     def agregador(self, request_iterator, context):
-        pass
+        #No se espera a que el cliente empuje todos
+        #mientras los está enviando, los va empujando
+        for elemento in request_iterator:
+            Comunicador.folio += 1
+            Comunicador.elementos[Comunicador.folio] = comunicaciones_pb2.Elemento(
+                id =  Comunicador.folio,
+                descripcion = elemento.descripcion)
+        return comunicaciones_pb2.Status(success=True)
+    
+def run():
+    puerto = "50051"
+    servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    comunicaciones_pb2_grpc.add_ComunicadorServicer_to_server(Comunicador(),servidor)
+    servidor.add_insecure_port("[::]:"+puerto)
+    print("Arrancando servidor:")
+    servidor.start()
+    servidor.wait_for_termination()
+        
+if __name__ == "main":
+    run()
