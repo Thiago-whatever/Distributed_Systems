b6251d4b1afca5be0613f40fb47a8b4f5b968926
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-04-08 19:05:18 -0600

    Cotizacion params datos carro

--- LabWeb_SegurosAuto/scripts/datosCarro.js
+++ LabWeb_SegurosAuto/scripts/datosCarro.js
@@ -1,13 +1,21 @@
 
 window.onload = function () {
-    const params = new URLSearchParams(window.location.search);    console.log(params);
-
-    
+    const params = new URLSearchParams(window.location.search);
     document.getElementById("estimado").textContent = `${params.get('nombre')} ${params.get('apellido')}`;
     document.getElementById("genero").textContent = params.get('genero');
     document.getElementById("edad").textContent = params.get('edad');
     document.getElementById("estado").textContent = params.get('estado');
-
-    
 };
 
+document.getElementById("enviar").onsubmit = function(event){
+    event.preventDefault();
+    const marca = document.getElementById('marca').value;
+    const placas = document.getElementById('placas').value;
+    const modelo = document.getElementById('modelo').value;
+
+    const queryParams = new URLSearchParams({
+        marca, placas, modelo
+    });
+
+    window.location.href = 'cotizacion.html?' + queryParams.toString();
+}
