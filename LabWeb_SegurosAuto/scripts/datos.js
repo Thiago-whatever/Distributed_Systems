39b15ab81e16844da8bb3f04ffc0bffb8eed8a4f
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-04-08 18:28:05 -0600

    abre pagina nueva con parametros

---
+++ LabWeb_SegurosAuto/scripts/datos.js
@@ -0,0 +1,14 @@
+document.getElementById().onsubmit = function(event){
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const genero = document.getElementById('genero').value;
+    const edad = document.getElementById('edad').value;
+    const estado = document.getElementById('estado').value;
+
+    const queryParams = new URLSearchParams({
+        nombre, apellido, genero, edad, estado
+    });
+
+    window.location.href = 'carro.html?' + queryParams.toString();
+}
