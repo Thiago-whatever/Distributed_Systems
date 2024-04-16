578e9b904a129a405ee9615cb48a0d9c173a1467
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-04-15 19:22:18 -0600

    Se agregan parametros en el index para hacer la function exchange; se implementa funcionalidad de exchange en el servidor

--- CasaCambio_RESTful/casaCam/app.py
+++ CasaCambio_RESTful/casaCam/app.py
@@ -5,6 +5,27 @@ import pickle
 app = Flask(__name__)
 modelo = pickle.load(open('modelo.pkl','rb'))
 
+@app.route('/exchange', methods=['GET'])
+def exchange():
+    presentCurrency = str(request.args["currency1"])
+    futureCurrency = str(request.args["currency2"])
+    amount = int(request.args["presentAmount"])
+
+    exchange_rates_USD = {
+        "USA": 1.0,
+        "Mexico": 16.0,
+        "Britain": 0.8,
+        "Canada": 1.38
+    }
+
+    if(presentCurrency == "USA"):
+        resultado = amount * exchange_rates_USD[futureCurrency]
+    else:
+        presentCurrencyUSD = amount / exchange_rates_USD[presentCurrency]
+        resultado = presentCurrencyUSD * exchange_rates_USD[futureCurrency]
+
+    return str(resultado)
+
 @app.route('/cambio', methods=['POST'])
 def cambio():
     presentCurrency = str(request.form["presentCurrency"])
