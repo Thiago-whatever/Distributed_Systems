201283cd529c0eab874139a64ffdfcf5132acbae
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-04-24 19:23:03 -0600

    Se agrega detalles.html y se modifica index en views

--- Django/mywebsite/myfirstapp/views.py
+++ Django/mywebsite/myfirstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Estudiante, Carrera
+from django.template import loader
 
 
 # Create your views here.
@@ -10,14 +11,18 @@ def index(request):
 
 def detalles(request, estudiante_id):
     return HttpResponse("Detalles del estudiante %s." % estudiante_id)
+
 def carreras(request, estudiante_id):
     return HttpResponse("Carreras del estudiante %s." % estudiante_id)
+
 def agrega_carrera(request, estudiante_id):
     return HttpResponse("Agregando una carrera a %s." % estudiante_id)
+
 def index(request):
     estudiantes = Estudiante.objects.order_by("nombre")
-    lista = ""
-    for e in estudiantes:
-        lista += e.__str__()+", "
-    return HttpResponse(lista)
+    context = {
+    "estudiantes": estudiantes,
+    }
+    return render(request, "myfirstapp/index.html", context)
+
 
