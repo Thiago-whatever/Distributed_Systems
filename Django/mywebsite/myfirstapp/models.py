afefee8536df2b19e216d8f010d5a476628fa617
Santiago Flores <85588937+Thiago-whatever@users.noreply.github.com>
2024-04-24 18:36:22 -0600

    Se agrega funcionalidad a los modelos estudiante y carrera

--- Django/mywebsite/myfirstapp/models.py
+++ Django/mywebsite/myfirstapp/models.py
@@ -8,6 +8,14 @@ class Estudiante(models.Model):
     edad = models.IntegerField(default=0)
     promedio = models.FloatField(default=9.99)
     foraneo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellidos
+    
+    def aspira_a_beca(self):
+        return self.promedio >= 9
+
+
 class Carrera(models.Model):
     LICENCIATURA = 1
     INGENIERIA = 2
@@ -18,3 +26,6 @@ class Carrera(models.Model):
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     tipo = models.IntegerField(choices=OPCIONES_TIPO, null=True, blank=True)
     nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.tipo) + " " + self.nombre
